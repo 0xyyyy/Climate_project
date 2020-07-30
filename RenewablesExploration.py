11de28057a6e34570d47c08f94b2c713b822f76f
Michael Cherkassky <MJCherkassky@gmail.com>
2020-07-30 18:42:18 -0500

    Uploading images and extraction file

--- RenewablesExploration.py
+++ RenewablesExploration.py
@@ -1,27 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 
+filename="bp-stats-review-2020-all-data.xlsx"
 # Load, Clean, Rename, Fillna, Dropna, Reset index
-RenCon_df = pd.read_excel("bp-stats-review-2020-all-data.xlsx", sheet_name= "Renewables Consumption - EJ", header=2 )[1:-9].rename(columns={"Exajoules (input-equivalent)": "Country"}).dropna(axis="index", how="all").fillna(0).set_index("Country")
-
-# Take out Regional values from csv rowsLittle more cleaning
-TotalRegion_df=RenCon_df[RenCon_df.index.str.startswith("Total")]
-TotalRegion_df.index.name = "Region"
-RegionalPlotData=TotalRegion_df.drop(["2019.2", "2008-18", "2019.1"], axis=1)
-RegionalPlotData
-
-
-# Plotting Regional Time Series Data
-Transposed=RegionalPlotData.transpose().drop(index=["Unnamed: 59", "Unnamed: 60"]) #Transpose for spaghetti plot data maniuplation
-
-# Convert values to arrays for matplotlib intake
-x=np.asarray(Transposed.index)
-y=np.asarray(Transposed.values)
-# For each Region(column) plot a separate line based on the numpy arrays, format plot
-for column in RegionalPlotData:
-    plt.plot(x, y, marker='', linewidth=1, alpha=0.9, label=column)
-    plt.title("Renewable Energy Consumption Through The Years")
-    plt.xlabel("Years")
-    plt.ylabel("Consumption (Exajoules)")
-    plt.legend(Transposed.columns)
+#All pages are formatted the same so we can use same execution for each page we read through pandas
+RenCon_df = pd.read_excel(filename, sheet_name= "Renewables Consumption - EJ", header=2)[1:-9].rename(columns={"Exajoules (input-equivalent)": "Country"}).dropna(axis="index", how="all").fillna(0).set_index("Country")
+RenGen_df=pd.read_excel(filename, sheet_name="Renewables Power - Twh", header=2)[1:-9].rename(columns={"Terawatt-hours": "Country"}).dropna(axis="index", how="all").fillna(0).fillna(0).set_index("Country")

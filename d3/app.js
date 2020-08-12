b3f9121a3363205645da158bf82de3d8cdc80b5d
Meera <meera@Meeras-Air-2.home>
2020-08-11 22:29:31 -0500

    rough d3 renew

---
+++ d3/app.js
@@ -0,0 +1,122 @@
+var svgWidth = 960;
+var svgHeight = 900;
+
+var margin = {
+  top: 40,
+  right: 100,
+  bottom: 200,
+  left: 130
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("totalworld.csv").then(function(renewData) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    renewData.forEach(function(data) {
+      data.year = +data.year;
+      data.total_world = +data.total_world;
+    });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([1965, 2020])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, 3000])
+      .range([height, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(renewData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.year))
+    .attr("cy", d => yLinearScale(d.total_world))
+    .attr("r", "15")
+    .attr("fill", "turquoise")
+    .attr("opacity", ".5");
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`Year: ${d.year}<br>Rate: ${d.total_world}`);
+      });
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "30px")  
+        .style("fill", "slategray")
+        .style("font-family", "Times New Roman")
+        .text("Renewables");
+
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .style("fill", "slategray")
+      .style("font-size", "20px")
+      .style("font-family", "Times New Roman")
+      .text("Renewable Rate");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .style("fill", "slategray")
+      .style("font-size", "20px")
+      .style("font-family", "Times New Roman")
+      .text("Year");
+  }).catch(function(error) {
+    console.log(error);
+  });

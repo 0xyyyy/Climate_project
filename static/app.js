b461c8b8a4e22d98a844115bfae6e96c8f92738e
mike malleus <apprentissage77@gmail.com>
2020-08-17 14:27:23 -0500

    embedding plotly

--- static/app.js
+++ static/app.js
@@ -8,127 +8,127 @@ for (let i = 0; i < logo.length; i++)
 
 
 
-// D3 code block
-var url = {
-    greenhouseGas_url: "http://127.0.0.1:5000/api/v1.0/greenhouseGas",
-    bp_url: "http://127.0.0.1:5000/api/v1.0/bpdata",
-    oil_url: "http://127.0.0.1:5000/api/v1.0/tempOil"
-}
+    // D3 code block
+    var url = {
+        greenhouseGas_url: "http://127.0.0.1:5000/api/v1.0/greenhouseGas",
+        bp_url: "http://127.0.0.1:5000/api/v1.0/bpdata",
+        oil_url: "http://127.0.0.1:5000/api/v1.0/tempOil"
+    }
 
-const greenhouseGas_df = d3.json(url.greenhouseGas_url).then(data=> console.log(data))
+        const greenhouseGas_df = d3.json(url.greenhouseGas_url).then(data=> console.log(data))
 
-const bp_df = d3.json(url.bp_url).then(data=> console.log(data))
+        const bp_df = d3.json(url.bp_url).then(data=> console.log(data))
 
-// svg container 
-var height = 600;
-var width = 1000;
-
-
-// margins 
-var margins = {
-    top: 50, 
-    right: 50, 
-    bottom: 50, 
-    left: 80
-}
-
-// chart area minus margins 
-var chartHeight = height - margins.top - margins.bottom;
-var chartWidth = width - margins.left - margins.right;
-
-//create svg container 
-var svg = d3.select("#scatter").append("svg")
-    .attr("height", height)
-    .attr("width", width);
-
-var chartGroup = svg.append('g')
-    .attr('transform', `translate(${margins.left}, ${margins.top})`)
-
-d3.json(url.oil_url).then(function (oilData) {
-    oilData.forEach(function (data) {
-        data.ID = +data.ID
-        data.Avg_Temp = +data.Avg_Temp
-        data.Year = +data.Year
-        data.Thousand_Barrels_Daily = +data.Thousand_Barrels_Daily
-    })
-    console.log(oilData)
-
-    var xLinearScale = d3.scaleLinear()
-    .domain([10000, d3.max(oilData, d => d.Thousand_Barrels_Daily)])
-    .range([0, chartWidth]);
-
-    var yLinearScale = d3.scaleLinear()
-    .domain([7.5, d3.max(oilData, d => d.Avg_Temp)])
-    .range([chartHeight, 0]);
-
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
-
-    chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
-
-    chartGroup.append("g")
-    .call(leftAxis);
-
-    var circlesGroup = chartGroup.selectAll("circle")
-    .data(oilData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.Thousand_Barrels_Daily))
-    .attr("cy", d => yLinearScale(d.Avg_Temp))
-    .attr('class', 'circle')
-    .attr("r", "14")
-    .attr("opacity", "0.7")
-    
-
-    var textGroup = chartGroup.selectAll("yearText")
-        .data(oilData)
-        .enter()
-        .append('text')
-        .attr('x', d => xLinearScale(d.Thousand_Barrels_Daily))
-        .attr('y', d => yLinearScale(d.Avg_Temp))
-        .text(d => d.Year)
-        .attr('class', 'yearText')
-        .attr('font-size', '11px')
-        .attr('text-anchor', 'middle')
-        .attr("dy", ".3em");
-       
+        // svg container 
+        var height = 600;
+        var width = 1000;
 
 
-            // // y axis gp ADDED
-            var yLabelsGp = chartGroup
-            .append('g')
-            .attr('transform', `translate(-20, ${chartHeight / 2})`)
+        // margins 
+        var margins = {
+            top: 50, 
+            right: 50, 
+            bottom: 50, 
+            left: 80
+        }
 
-            // // Append yAxis ADDED
-            var tempLabel = yLabelsGp
-            .append('text')
-            .attr('transform', 'rotate(-90)')
-            .attr('y', -45)
-            .attr('x', 0)
-            .attr('dy', '1em')
-            .classed('axis-text', true)
-            .classed('active', true)
-            .text('Average Temperature by Year US')
+        // chart area minus margins 
+        var chartHeight = height - margins.top - margins.bottom;
+        var chartWidth = width - margins.left - margins.right;
+
+        //create svg container 
+        var svg = d3.select("#scatter").append("svg")
+            .attr("height", height)
+            .attr("width", width);
+
+        var chartGroup = svg.append('g')
+            .attr('transform', `translate(${margins.left}, ${margins.top})`)
+
+        d3.json(url.oil_url).then(function (oilData) {
+            oilData.forEach(function (data) {
+                data.ID = +data.ID
+                data.Avg_Temp = +data.Avg_Temp
+                data.Year = +data.Year
+                data.Thousand_Barrels_Daily = +data.Thousand_Barrels_Daily
+            })
+            console.log(oilData)
+
+            var xLinearScale = d3.scaleLinear()
+            .domain([10000, d3.max(oilData, d => d.Thousand_Barrels_Daily)])
+            .range([0, chartWidth]);
+
+            var yLinearScale = d3.scaleLinear()
+            .domain([7.5, d3.max(oilData, d => d.Avg_Temp)])
+            .range([chartHeight, 0]);
+
+            var bottomAxis = d3.axisBottom(xLinearScale);
+            var leftAxis = d3.axisLeft(yLinearScale);
+
+            chartGroup.append("g")
+            .attr("transform", `translate(0, ${chartHeight})`)
+            .call(bottomAxis);
+
+            chartGroup.append("g")
+            .call(leftAxis);
+
+            var circlesGroup = chartGroup.selectAll("circle")
+            .data(oilData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xLinearScale(d.Thousand_Barrels_Daily))
+            .attr("cy", d => yLinearScale(d.Avg_Temp))
+            .attr('class', 'circle')
+            .attr("r", "14")
+            .attr("opacity", "0.7")
+            
+
+            var textGroup = chartGroup.selectAll("yearText")
+                .data(oilData)
+                .enter()
+                .append('text')
+                .attr('x', d => xLinearScale(d.Thousand_Barrels_Daily))
+                .attr('y', d => yLinearScale(d.Avg_Temp))
+                .text(d => d.Year)
+                .attr('class', 'yearText')
+                .attr('font-size', '11px')
+                .attr('text-anchor', 'middle')
+                .attr("dy", ".3em");
+            
+
+
+                    // // y axis gp ADDED
+                    var yLabelsGp = chartGroup
+                    .append('g')
+                    .attr('transform', `translate(-20, ${chartHeight / 2})`)
+
+                    // // Append yAxis ADDED
+                    var tempLabel = yLabelsGp
+                    .append('text')
+                    .attr('transform', 'rotate(-90)')
+                    .attr('y', -45)
+                    .attr('x', 0)
+                    .attr('dy', '1em')
+                    .classed('axis-text', true)
+                    .classed('active', true)
+                    .text('Average Temperature by Year US')
 
 
 
-    
-    // chartGroup.append('text')
-    //     .attr("transform", `rotate(-90)`)
-    //     .attr("y", 0 - margins.left + 20)
-    //     .attr("x", 0 - (chartHeight/2))
-    //     .attr("dy", "1em")
-    //     .attr('class', 'axisText')
-    //     .text("Average Temperature by Year US")
-    
-    chartGroup.append("text")
-        .attr('transform', `translate(400, ${chartHeight + 40})`)
-        .attr("class", "axisText")
-        .classed('active', true)
-        .text("Barrels Consumed Daily in Thousands")
-})
+            
+            // chartGroup.append('text')
+            //     .attr("transform", `rotate(-90)`)
+            //     .attr("y", 0 - margins.left + 20)
+            //     .attr("x", 0 - (chartHeight/2))
+            //     .attr("dy", "1em")
+            //     .attr('class', 'axisText')
+            //     .text("Average Temperature by Year US")
+            
+            chartGroup.append("text")
+                .attr('transform', `translate(400, ${chartHeight + 40})`)
+                .attr("class", "axisText")
+                .classed('active', true)
+                .text("Barrels Consumed Daily in Thousands");
+        })
 
 
 
